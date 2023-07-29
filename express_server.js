31ef023b639b621240939045a052d09213872037
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-29 14:41:43 -0600

    Added urlencoded

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -24,6 +25,10 @@ app.get('/hello', (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
