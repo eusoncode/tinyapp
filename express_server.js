ee7fbac9d7d56716f9a3169122340001bef42873
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-29 20:04:51 -0600

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -19,20 +19,6 @@ function generateRandomString() {
   }
   return result;
 };
-const shortURL = generateRandomString();
-
-
-app.get('/', (req, res) => {
-  res.send('Hello!')
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -40,21 +26,32 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  console.log(req.body);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+  console.log(urlDatabase);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
-    longURL: "http://www.lighthouselabs.ca"/* What goes here? */ 
+    longURL: urlDatabase[this.id]
   };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:id", (req, res) => {  
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+
+  if (!shortURL) {
+    res.send(`Oops! The ${shortURL} id you requested does not exist`)
+  }
+  res.redirect(longURL);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
