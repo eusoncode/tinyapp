9f49888d8ba02cc5c18f4b9aac69355b63182508
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-29 21:42:37 -0600

    Add POST route for /urls/:id/delete to remove URLS

--- express_server.js
+++ express_server.js
@@ -1,15 +1,19 @@
+// Set up basic web server using express
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // Define default port 8080
+app.use(express.urlencoded({ extended: true })); // use middleware to convert data to human readable form
 
-app.use(express.urlencoded({extended: true}));
+// set view engine to EJS
 app.set('view engine', 'ejs');
 
+// Create a url database to store  and access short and long url for the app
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Create a string of 6 random alphanumeric characters that will be used as short URL
 function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -20,19 +24,20 @@ function generateRandomString() {
   return result;
 };
 
+// Get Routes
+
+// Routes to the index template when /urls is called
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`)
-  console.log(urlDatabase);
+// Route to the new url template
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
+// Route to the show template
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -41,16 +46,36 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Route to redirect the short URL to the long URL
 app.get("/u/:id", (req, res) => {  
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
 
-  if (!shortURL) {
+  if (!shortURL) { //If use inputs short URL shorter than 6 characters
     res.send(`Oops! The ${shortURL} id you requested does not exist`)
   }
   res.redirect(longURL);
 });
 
+// POST Routes
+
+// Route for creating new url
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+  console.log(urlDatabase);
+});
+
+// Route for delete a url
+app.post('/urls/:id/delete', (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls')
+});
+
+// Listening on PORT 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
